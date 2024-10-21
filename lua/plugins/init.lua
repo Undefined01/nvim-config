84ccb5a4ea2fe1f67509fc27146b56bbf96941e6
Undefined01 <amoscr@163.com>
2024-10-21 13:14:03 +0800

    Update

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -8,7 +8,6 @@ return {
   {
     "neovim/nvim-lspconfig",
     config = function()
-      require("nvchad.configs.lspconfig").defaults()
       require "configs.lspconfig"
     end,
   },
@@ -24,12 +23,6 @@ return {
   -- 	},
   -- },
   --
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      require "configs.lspconfig"
-    end
-  },
   {
     "nvim-treesitter/nvim-treesitter",
     opts = {
